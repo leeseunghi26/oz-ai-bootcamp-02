5b7d7a636fa7b556b8e8bedb2742269badf52085
leeseunghi26 <leeseunghi26@naver.com>
2026-02-17 22:11:16 +0900

    자바 계산기 실습 완료

---
+++ null.js
@@ -0,0 +1,8 @@
+// null=값이 비어 있음
+
+//예: 복권 추첨
+
+let selectUser = null;
+
+console.log(selecteUser);
+console.log(typeof selecteUser);
---
+++ var.js
@@ -0,0 +1,9 @@
+let age = 20;
+age = 30;
+console.log(age);
+
+
+// constnat
+const name = "alex";
+username = "bob";
+console.log(username);
