ce317d07089f846415ff42e167f7778677a96e69
leeseunghi26 <leeseunghi26@naver.com>
2026-03-06 12:00:08 +0900

    docker upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from sqlalchemy import text
+
+from database import SessionFactory
+
+
+app = FastAPI()
+
+
+@app.get("/users")
+async def get_users_handler():
+    return {"ping":"pong"}
+
+
+
+@app.get("/users")
+async def get_users_handler():
+
+    with SessionFactory() as session:
+        stmt = text("SELECT * FROM user ;")
+        result = session.execute(stmt).mappings().all()
+    return {"result": result}
